c14fcd08f969a918ec472945237d3d9224eb5326
sta-alt290 <satriatrunk@gmail.com>
2025-01-01 15:14:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "z7JyGbKA#Rg_BVv8XAa7iq7vKCsfteER89zGbXaIIJBELVZvgUKE", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "Sno0xbyK#VytO00c1ztYwFI-DYzwSA3_u5kmjXNZm-G8Aw7w3Mq0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
